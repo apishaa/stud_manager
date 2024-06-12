782b066cb0367a6bfaf05871dfcb37d1e3f427ca
apishaa <apishaafdo@gmail.com>
2024-06-12 08:47:59 +0530

    reupload

--- func.py
+++ func.py
@@ -16,3 +16,20 @@ def registeration(username,age,address,course,duration):
     
     write_json(data)
     
+def delete_stud(id):
+        data=read_json()
+        name=""
+        for stud in data["students"]:
+            if stud["sno"] == int(id):
+                name=stud["name"]
+                data["students"].remove(stud)
+        i=1
+        for stud in data["students"]:
+            stud["sno"]=i
+            i+=1
+        write_json(data)
+        return name
+                    
+                
+        
+    
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ app=Flask(__name__)
 @app.route("/",methods=["POST","GET"])
 
 def home():
+    msg=""
     if request.method=="POST":
+        msg=request.form["username"]
         # print(request.form["username"])
         # print(request.form["age"])
         # print(request.form["add"])
@@ -12,7 +14,15 @@ def home():
         # print(request.form["duration"])
         registeration(request.form["username"],request.form["age"],request.form["add"],request.form["course"],request.form["duration"])
     data=read_json()    
-    return render_template("index.html",stud=data["students"])
+    return render_template("index.html",stud=data["students"],msg=msg)
+
+@app.route("/delete/<id>",methods=["POST","GET"])
+def delete(id):
+    name=delete_stud(id)
+    data=read_json()
+    return render_template("index.html",stud=data["students"],name=name)
+    
+    
 
 if __name__=="__main__":
     app.run(debug=True)
